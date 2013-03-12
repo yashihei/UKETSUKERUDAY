e25282425e7d244530712041b20402674af68e93
Kobayashi Ryuhei <yashihei@gmail.com>
2013-03-12 17:00:56 +0900

    Add radio button for participant info.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,2 +1,10 @@
 class Participant < ActiveRecord::Base
+	module Arrival; end
+	Arrival::YET = 0
+	Arrival::ARRIVED = 1
+
+	ARRIVAL_NAME = {
+		Arrival::YET => "KITENAI",
+		Arrival::ARRIVED => "KITERU"
+	}
 end
