9b0c6646bc985899edb650e79e6181db0f272779
becyn <yucalcium.c6@gmail.com>
2013-03-13 16:47:17 +0900

    Add seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+participants = Participant.create([{ name: 'becyn' , arrival: '1', BINGO: '1', party: '1', occupation: '0', remarks: 'aiueo' }])
+participants = Participant.create([{ name: '92thunder' , arrival: '0', BINGO: '1', party: '1', occupation: '0', remarks: 'aiueo' }])
+participants = Participant.create([{ name: 'miyay', arrival: '0', BINGO: '1', party: '1', occupation: '1', remarks: 'aiueo' }])
+participants = Participant.create([{ name: 'kyoro353', arrival: '1', BINGO: '1', party: '1', occupation: '1', remarks: 'aiueo' }])
